d047c595d03c802f0e6880603400f971568d2599
Ethan Li <lietk12@gmail.com>
2020-09-24 02:35:34 -0700

    Disable Alt+Tab keyboard shortcut on RPi (#153)
    * Show splash screen for longer on boot and hide console messages on shutdown
    
    * Add LXDE configuration to disable Alt+Tab

--- deploy/scripts/configure_kiosk.sh
+++ deploy/scripts/configure_kiosk.sh
@@ -6,8 +6,17 @@ sudo apt install unclutter -y
 script_dir=$(pwd)
 
 cd ~/
+
+# Set up autostart
+
 mkdir -p .config/lxsession/LXDE-pi
 touch .config/lxsession/LXDE-pi/autostart
 
 cat /etc/xdg/lxsession/LXDE-pi/autostart > .config/lxsession/LXDE-pi/autostart
 cat $script_dir/configs/screen_config.txt >> .config/lxsession/LXDE-pi/autostart
+
+# Disable Alt+Tab (causes buggy behavior in Chromium with touchscreen events)
+
+mkdir -p .config/openbox
+touch .config/openbox/lxde-rc.xml
+cat $script_dir/configs/lxde-pi-rc.xml > .config/openbox/lxde-pi-rc.xml
