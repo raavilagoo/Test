f8ef4700b9ce0b74a81e55de6bfd2f1e13c8fc56
Praneet <praneetbomma@gmail.com>
2020-09-25 07:48:45 +0530

    disabled temporary environment creation before poetry install (#158)

--- deploy/scripts/poetry.sh
+++ deploy/scripts/poetry.sh
@@ -8,4 +8,5 @@ ventserver_env="$HOME/.pyenv/versions/3.7.7/envs/ventserver/bin/python"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 
 cd ~/pufferfish-vent-software/backend
+$poetry config virtualenvs.create false
 $ventserver_env $poetry install
